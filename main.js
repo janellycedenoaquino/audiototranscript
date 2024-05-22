e0c52d3e08b5a8f2af023ecfd45dbcca30b61939
janellycedenoaquino <janellycedenoo@gmail.com>
2024-05-22 14:50:13 -0400

    used a new instance of openai with my openai key. added a function that makes a call to openai using whisper and console logs the transcript of the video provided.

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const OpenAI = require("openai");
 const fs = require("fs");
 require('dotenv').config();
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const audioAPICall = async () =>{
+    const transcription = await openai.audio.transcription.create({
+        file:fs.createReadStream("nameOfLocalAudio"),
+        model:"whisper-1"
+    })
+    console.log(transcription);
+}
+
+audioAPICall();
